7cc69939fceb5a8088073ae8d656dbf40c05dfa1
KryvaSofia <119862637+SofiaK031@users.noreply.github.com>
2023-11-30 19:07:57 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -52,7 +52,7 @@ document.addEventListener("DOMContentLoaded", () => {
         $("#user-word").focus();
     }
     //Початок виконання
-    $("input[type='radio']").checkboxradio({ icon: true });
+    /*$("input[type='radio']").checkboxradio({ icon: true });*/
     numberOfCards = Math.floor(wordsToTranslate.lenth * 0.35)
     init(numberOfCards);
 
@@ -70,7 +70,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 numberOfCards = wordsToTranslate.lenth;
                 init(numberOfCards);
                 break;
-            default: alert("Error occured!");
+            default: alert("Error!");
                 break;
         }
     });
